53e6abcd14d3f66a74c8da7952b7296fa8a319af
Yannickqb <yanni.ledermann@gmail.com>
2023-07-07 15:12:53 +0200

    some problems fixed

--- restart/components/EventDetails.js
+++ restart/components/EventDetails.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
@@ -17,6 +16,26 @@ const EventDetails = ({ onBackPress, event, updateEvent }) => {
         <Text style={styles.details}>Event Details</Text>
         <Text style={styles.description}>Event Type: {event.title}</Text>
         <Text style={styles.description}>Date: {event.date}</Text>
+
+        <Text style={styles.tableHeader}>Starting Offense</Text>
+        <View style={styles.table}>
+          <Text style={styles.tableRow}>Player 1</Text>
+          <Text style={styles.tableRow}>Player 2</Text>
+          <Text style={styles.tableRow}>Player 3</Text>
+          <Text style={styles.tableRow}>Player 3</Text>
+          <Text style={styles.tableRow}>Player 3</Text>
+          <Text style={styles.tableRow}>Player 3</Text>
+          <Text style={styles.tableRow}>Player 3</Text>
+
+        </View>
+
+        <Text style={styles.tableHeader}>Starting Defense</Text>
+        <View style={styles.table}>
+          <Text style={styles.tableRow}>Player 4</Text>
+          <Text style={styles.tableRow}>Player 5</Text>
+          <Text style={styles.tableRow}>Player 6</Text>
+        </View>
+
         <Button title="Mark Completed" onPress={handleEventUpdate} />
         <Button title="Go Back" onPress={onBackPress} />
       </View>
@@ -57,6 +76,26 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
     marginBottom: 10,
+    textAlign: 'center',
+  },
+  tableHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  table: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 20,
+  },
+  tableRow: {
+    fontSize: 16,
+    marginBottom: 5,
     textAlign: 'center',
   },
 });
